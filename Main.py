a372308512f82ceeeaa5d7ce4f51e47cdc593d48
Benjamin Lincoln <support@lincolnapps.net>
2018-09-15 22:16:44 +1000

    Added missing word removal check

--- Main.py
+++ Main.py
@@ -13,6 +13,14 @@ while mainLoopActive:
 	else:
 		pattern = re.compile('([A-Z][a-z]+)')
 		capitalWords = re.findall(pattern, userInput)
+		
+		for word in wordsToIgnore:
+			for capitalWord in capitalWords:
+				if(capitalWord.lower() == word.lower()):
+					capitalWords.remove(capitalWord)
+		
+		print()
+		print("Found words are:")
 		print(capitalWords)
-
+	print()
 print("exited")
